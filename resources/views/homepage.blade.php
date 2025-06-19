4ee7d3d9176f1932739014c0d63fbea99c1c5f36
unknown <hanifardiansyah002@gmail.com>
2025-06-19 23:44:16 +0700

    homepage edit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beranda - Prediksi Penyakit Jantung</title>
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white font-sans">
